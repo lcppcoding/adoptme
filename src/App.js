5f436b7ca53065674a0dfc046d5a8ecc54933249
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-26 14:22:31 -0300

    project set up, react working on dev server

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    // ...[1, 2, 3, 4].map(i => React.createElement("h2", {}, i)),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Paulistinha",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaton Terrier",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
