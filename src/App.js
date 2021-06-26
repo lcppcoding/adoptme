4e46b81b48275eab8147e6c1d7f57d95cc8a21e3
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-26 16:10:28 -0300

    jsx setup done

--- src/App.js
+++ src/App.js
@@ -2,26 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     // ...[1, 2, 3, 4].map(i => React.createElement("h2", {}, i)),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Paulistinha",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Sudo",
+//       animal: "Dog",
+//       breed: "Wheaton Terrier",
+//     }),
+//   ]);
+// };
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    // ...[1, 2, 3, 4].map(i => React.createElement("h2", {}, i)),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Paulistinha",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaton Terrier",
-    }),
-  ]);
+  return (
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+  </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
