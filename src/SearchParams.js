a7fa7d25be25c8eb7046ba61402de9597ab222f6
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-29 15:29:26 -0300

    themes and theme context added

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import ThemeContext from './ThemeContext';
 import useBreedList from './useBreedList';
 import Results from './Results';
 
@@ -10,6 +11,7 @@ const SearchParams = () => {
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
     requestPets();
@@ -88,7 +90,20 @@ const SearchParams = () => {
               }
           </select>
         </label>
-        <button>Submit</button>
+        <label htmlFor="theme">
+          theme
+          <select
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+            >
+              <option value="darkblue">Dark Blue</option>
+              <option value="peru">Peru</option>
+              <option value="green">Green</option>
+              <option value="red">Red</option>
+            </select>
+        </label>
+        <button style={ { backgroundColor: theme } }>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
