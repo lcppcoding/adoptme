f12adefa3cf2e80f19b9d07983fd931d297c8729
Lucas Pessoa <lcppcoding@gmail.com>
2021-07-03 22:17:46 -0300

    finished tailwind styling on most pages, details page to do

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -49,18 +49,20 @@ const SearchParams = () => {
           requestPets();
         }}
       >
-        <label htmlFor="location">
+        <label className="search-label" htmlFor="location">
           Location
           <input
+            className="search-control"
             id="location"
             value={location}
             onChange={updateLocation} 
             laceholder="Location"
           />
         </label>
-        <label htmlFor="animal">
+        <label className="search-label" htmlFor="animal">
           Animal
           <select
+            className="search-control"
             id="animal"
             value={animal}
             onChange={updateAnimal}
@@ -76,9 +78,10 @@ const SearchParams = () => {
               }
           </select>
         </label>
-        <label htmlFor="breed">
+        <label className="search-label" htmlFor="breed">
           Breed
           <select
+            className="search-control disabled:opacity-50"
             id="breed"
             value={breed}
             onChange={updateBreed}
@@ -94,9 +97,10 @@ const SearchParams = () => {
               }
           </select>
         </label>
-        <label htmlFor="theme">
+        <label className="search-label" htmlFor="theme">
           theme
           <select
+            className="search-control"
             value={theme}
             onChange={e => setTheme(e.target.value)}
             onBlur={e => setTheme(e.target.value)}
@@ -107,7 +111,7 @@ const SearchParams = () => {
               <option value="red">Red</option>
             </select>
         </label>
-        <button style={ { backgroundColor: theme } }>Submit</button>
+        <button className="rounded px-6 py-2 text-white hover:opacity-50 border-none" style={ { backgroundColor: theme } }>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
